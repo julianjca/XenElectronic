583950490ea5869edb2c3681fec80aeb5df1582c
Julian Anderson <julian@jumpcut.com>
2021-02-06 11:08:37 +0700

    updating migration and seed for products and categories

--- backend/__tests__/routes.test.js
+++ backend/__tests__/routes.test.js
@@ -52,6 +52,77 @@ describe('Products Endpoints', () => {
     const res = await request(app).get('/products/1')
     expect(res.statusCode).toEqual(200)
     expect(res.body).toEqual({
+      "product": {
+        "id": 4,
+        "name": "Product 1",
+        "price": "100.00",
+        "category_id": 1,
+        "category_name": "Category 1"
+      }
+    })
+  })
+
+  it('should update product by id 1', async () => {
+    const res = await request(app).put('/products/1').send({
+      name: 'test',
+      price: 100,
+    })
+    expect(res.statusCode).toEqual(200)
+    expect(res.body).toEqual({
+      message: 'Success updating product.',
+    })
+  })
+
+  it('should deleting product by id 1', async () => {
+    const res = await request(app).delete('/products/1')
+    
+    expect(res.statusCode).toEqual(200)
+    expect(res.body).toEqual({
+      message: 'Success deleting product.',
+    })
+  })
+})
+
+describe('Categories Endpoints', () => {
+  beforeEach((done) => knex.migrate.rollback()
+  .then(() => knex.migrate.latest())
+  .then(() => knex.seed.run())
+  .then(() => done())
+);
+
+  afterEach((done) => {
+    knex.migrate.rollback()
+    .then(() => {
+      done();
+    });
+  });
+
+  it('should create a new product', async () => {
+    const res = await request(app)
+      .post('/products')
+      .send({
+        name: 'test',
+      })
+    expect(res.statusCode).toEqual(200)
+    expect(res.body).toEqual({ message: 'Success adding category.' })
+  })
+
+  it('should get all products', async () => {
+    const res = await request(app).get('/products')
+    expect(res.statusCode).toEqual(200)
+    expect(res.body).toEqual( {
+      products: [
+        { id: 1, name: 'Category 1'},
+        { id: 2, name: 'Product 2'},
+        { id: 3, name: 'Product 3'}
+      ]
+    })
+  })
+
+  it('should get product by id 1', async () => {
+    const res = await request(app).get('/products/1')
+    expect(res.statusCode).toEqual(200)
+    expect(res.body).toEqual({
       product: { id: 1, name: 'Product 1', price: '100.00' },
     })
   })
--- backend/migrations/20210206081337_products_categories.js
+++ backend/migrations/20210206081337_products_categories.js
@@ -2,15 +2,13 @@
 exports.up = function(knex) {
   return Promise.all([
     knex.schema.createTable('categories', (table) => {
-      table.increments('id').primary();
-      table.string('name');
+      table.uuid('id').primary().unique().notNullable();      table.string('name');
     }),
     
     knex.schema.createTable('products', table => {
-      table.increments('id').primary
-      table.string('name')
+      table.uuid('id').primary().unique().notNullable();      table.string('name')
       table.decimal('price',15,2)
-      table.integer('category_id').unsigned()
+      table.uuid('category_id').unsigned()
           .references('id').on('categories');
     })
   ]) 
